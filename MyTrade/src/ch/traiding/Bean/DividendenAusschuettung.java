ef8027b67fb023c9c49e12450b4b67fa7da85a66
joshua.bloechliger <joshua.bloechliger@ZHR-WST-237.sanschoolsbsdom.local>
2016-05-24 11:04:31 +0200

    x

--- MyTrade/src/ch/traiding/Bean/DividendenAusschuettung.java
+++ MyTrade/src/ch/traiding/Bean/DividendenAusschuettung.java
@@ -1,19 +1,15 @@
 package ch.traiding.Bean;
 
 import javax.faces.application.FacesMessage;
+import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 
 @ManagedBean(name = "ausschuettung")
-@ViewScoped
+@ApplicationScoped
 public class DividendenAusschuettung {
 
-	public void ausschuetten() {
-		System.out.println("Wird ausgeschüttet");
-	}
-
-	public void confirmDividendenAusschuettung() {
+	public void dividendeAusschuetten() {
 		addMessage("Dividende", "Die Dividende wurde erfolgreich ausgeschüttet");
 	}
 
